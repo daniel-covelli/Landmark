87a4b3964471c190dbe4e9192e847a6a0124c59c
Daniel Covelli <57047397+daniel-covelli@users.noreply.github.com>
2022-11-13 12:07:40 -0800

    add mutability to json data

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
-        VStack {
+        ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
@@ -20,14 +25,15 @@ struct LandmarkDetail: View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
-            
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
-                    
                     Spacer()
                     Text(landmark.state)
                 }
@@ -41,7 +47,6 @@ struct LandmarkDetail: View {
                 Text(landmark.description)
             }
             .padding()
-            
         }
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
@@ -49,7 +54,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: ModelData().landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
     }
 }
--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -9,29 +9,29 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
+                .cornerRadius(5)
             Text(landmark.name)
-            
+
             Spacer()
-            
+
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .imageScale(.medium)
                     .foregroundColor(.yellow)
             }
         }
     }
-    
-    
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-    
+
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
